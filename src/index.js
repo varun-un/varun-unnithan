8e6c477ebfbf0b15750bc79073bf6257d3479857
varun_u <77634835+astroameba@users.noreply.github.com>
2021-04-15 18:21:52 -0400

    added 3D name

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.js";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+, document.getElementById("root"));
