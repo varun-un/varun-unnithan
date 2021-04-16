8079f5d2d273649fb3c6e87984a1aeea46d1f5cd
varun_u <77634835+astroameba@users.noreply.github.com>
2021-04-15 21:11:23 -0400

    added 3d name + scroll arrow

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Name3d from "./name3d.js";
+
+function home() {
+    return (
+        <div className="homePage">
+            <Name3d />
+            <a className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
+        </div>
+    )
+}
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.js";
+import Home from "./home.js";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Home />
     </React.StrictMode>
 , document.getElementById("root"));
