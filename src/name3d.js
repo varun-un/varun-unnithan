01773dc0bb297486988605ca3683c4e87d34da0b
varun_u <77634835+astroameba@users.noreply.github.com>
2021-04-17 13:45:52 -0400

    fixed rotation mechs

--- src/name3d.js
+++ src/name3d.js
@@ -18,19 +18,15 @@ const onSceneReady = (scene) => {
 
     var k = 5000;   //k for scale of radius refactor on window resize
 
-    var camera = new BABYLON.ArcRotateCamera("arcCamera", Math.PI / -2, Math.PI / 2, k / window.innerWidth + .85, BABYLON.Vector3.Zero(), scene);
+    var camera = new BABYLON.ArcRotateCamera("arcCamera", Math.PI / -2, Math.PI / 2, k / window.innerWidth + .5, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
-    // camera.angularSensibilityX = 9000;
-    // camera.angularSensibilityY = 9000;
-    // camera.upperAlphaLimit = BABYLON.Tools.ToRadians(20) + (Math.PI / -2);
-    // camera.upperBetaLimit = BABYLON.Tools.ToRadians(20) + (Math.PI / 2);
-    // camera.lowerAlphaLimit = BABYLON.Tools.ToRadians(-20) + (Math.PI / -2);
-    // camera.lowerBetaLimit = BABYLON.Tools.ToRadians(-20) + (Math.PI / 2);
-    // console.log(camera.radius)
-    // camera.upperRadiusLimit = k / window.innerWidth + .85;
-    // camera.lowerRadiusLimit = k / window.innerWidth + .85;
-    camera.lowerRadiusLimit = 1;
-    camera.wheelDeltaPercentage = .025;
+    camera.angularSensibilityX = 1500;
+    camera.angularSensibilityY = 1500;
+    camera.lowerBetaLimit = (0);
+    camera.upperBetaLimit = (2 * Math.PI / 3);
+    camera.upperRadiusLimit = k / window.innerWidth + .5;
+    camera.lowerRadiusLimit = k / window.innerWidth + .5;
+    camera.wheelDeltaPercentage = 0;
     camera.allowUpsideDown = true;
     camera.panningAxis = new BABYLON.Vector3(1,1,0);
     camera.panningInertia = .9;
@@ -38,17 +34,17 @@ const onSceneReady = (scene) => {
 
     //camera motion
     // document.addEventListener('mousemove', logKey);
-    function logKey(e) {
-        var xLoc = (e.clientX - (window.innerWidth / 2)) / (window.innerWidth / 2);
-        var yLoc = (e.clientY - (window.innerHeight / 2)) / (window.innerHeight / 2);
-        updateCam(xLoc, yLoc);
-    }
-    function updateCam(xLoc, yLoc){
-        var newAlpha = -1 * xLoc * BABYLON.Tools.ToRadians(10) + (Math.PI / -2);
-        var newBeta = -1 * yLoc * BABYLON.Tools.ToRadians(10) + (Math.PI / 2);
-        camera.alpha = newAlpha;
-        camera.beta = newBeta;
-    }   
+    // function logKey(e) {
+    //     var xLoc = (e.clientX - (window.innerWidth / 2)) / (window.innerWidth / 2);
+    //     var yLoc = (e.clientY - (window.innerHeight / 2)) / (window.innerHeight / 2);
+    //     updateCam(xLoc, yLoc);
+    // }
+    // function updateCam(xLoc, yLoc){
+    //     var newAlpha = -1 * xLoc * BABYLON.Tools.ToRadians(30) + (Math.PI / -2);
+    //     var newBeta = -1 * yLoc * BABYLON.Tools.ToRadians(30) + (Math.PI / 2);
+    //     camera.alpha = newAlpha;
+    //     camera.beta = newBeta;
+    // }   
 
     
     var hemiLight = new BABYLON.HemisphericLight("downLight", new BABYLON.Vector3(0,1,0), scene);
@@ -68,6 +64,8 @@ const onSceneReady = (scene) => {
     BABYLON.SceneLoader.ImportMesh("","", "./models/face.babylon", scene, function(newMeshes){
         newMeshes.forEach(function (mesh) {         //for each in the array of meshes imported
             hemiLight.includedOnlyMeshes.push(mesh);
+            mesh.position.y -= .1;
+            mesh.position.z += .2;
         });
     });    
 
@@ -112,6 +110,42 @@ const onSceneReady = (scene) => {
     if (window) {
         window.addEventListener("resize", resize);
     }
+
+    function showWorldAxis(size) {
+        var makeTextPlane = function(text, color, size) {
+            var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+            dynamicTexture.hasAlpha = true;
+            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color , "transparent", true);
+            var plane = BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+            plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+            plane.material.backFaceCulling = false;
+            plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+            plane.material.diffuseTexture = dynamicTexture;
+        return plane;
+            };
+        var axisX = BABYLON.Mesh.CreateLines("axisX", [ 
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), 
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+            ], scene);
+        axisX.color = new BABYLON.Color3(1, 0, 0);
+        var xChar = makeTextPlane("X", "red", size / 10);
+        xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+        var axisY = BABYLON.Mesh.CreateLines("axisY", [
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( -0.05 * size, size * 0.95, 0), 
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( 0.05 * size, size * 0.95, 0)
+            ], scene);
+        axisY.color = new BABYLON.Color3(0, 1, 0);
+        var yChar = makeTextPlane("Y", "green", size / 10);
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+        var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+            BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+            ], scene);
+        axisZ.color = new BABYLON.Color3(0, 0, 1);
+        var zChar = makeTextPlane("Z", "blue", size / 10);
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+    };
+    showWorldAxis(15);
 };
 
 var frame = 0;
